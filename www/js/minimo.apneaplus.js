3f79f01ea31cf4e085fc614e14db2b947680ad76
brandshocker <brandshocker@gmail.com>
2017-05-05 00:05:53 +0700

    Add files via upload

--- www/js/minimo.apneaplus.js
+++ www/js/minimo.apneaplus.js
@@ -1,45 +1,58 @@
-        document.addEventListener('deviceready', function () {
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            cordova.plugins.notification.local.on("click", function (notification) {
-                
-                var user = JSON.parse(notification.data);
-                navigator.notification.confirm(
-                    'Hello ' + user['username'] + ', Category selected ' + notification.id, // message
-                    onConfirm, // callback to invoke with index of button pressed
-                    'Saving data', // title
+onConfirm = function (buttonIndex) {
+    switch (buttonIndex) {
+        case 1:
+            alert('1');
+            break;
+        case 2:
+            alert('2');
+            break;
+    }
+}
+
+testNotif = function () {
+    var now = new Date().getTime(),
+        _5_sec_from_now = new Date(now + 5 * 1000);
+
+    var sound = "file://notification.mp3";
+
+    cordova.plugins.notification.local.schedule({
+        id: 1,
+        title: 'Apnea+',
+        text: 'Dont forget to train today ...',
+        at: _5_sec_from_now,
+        sound: sound,
+        badge: 1,
+        data: {
+            "username": "Novan"
+        }
+    })
+};
+
+init = function()
+{
+    log('Initialized');
+    
+    o('main-header').style.height = '70px';
+    o('main-subheader').style.height = '50px';
+}
+
+
+document.addEventListener('deviceready', function () {
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    cordova.plugins.notification.local.on("click", function (notification) {
+
+        var user = JSON.parse(notification.data);
+        navigator.notification.confirm(
+            'Hello ' + user['username'] + ', Category selected ' + notification.id, // message
+            onConfirm, // callback to invoke with index of button pressed
+            'Saving data', // title
                     ['Ok', 'Cancel'] // buttonLabels
-                );
-            });
+        );
+    });
 
-            function onConfirm(buttonIndex) {
-                switch(buttonIndex){
-                    case 1:
-                        alert('1');
-                        break;
-                    case 2:
-                        alert('2');
-                        break;
-                }
-            }
-
-            testNotif = function () {
-                var now = new Date().getTime(),
-                    _5_sec_from_now = new Date(now + 5 * 1000);
-
-                var sound = "file://notification.mp3";
-
-                cordova.plugins.notification.local.schedule({
-                    id: 1,
-                    title: 'Apnea+',
-                    text: 'Dont forget to train today ...',
-                    at: _5_sec_from_now,
-                    sound: sound,
-                    badge: 1,
-                    data: {
-                        "username" : "Novan"
-                    }
-                })
-            };
-            
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        });
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+});
--- www/js/minimo.core.js
+++ www/js/minimo.core.js
@@ -1,1 +1,10 @@
 // MINIMO CORE 1.1
+
+log = function(i)
+{
+    console.log(i);
+}
+
+o = function(id){
+    return document.getElementById(id);
+}
