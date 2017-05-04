1cee23c1ae55c0ee7373647b5babb7757b9f5acc
brandshocker <brandshocker@gmail.com>
2017-05-04 21:59:48 +0700

    Add files via upload

---
+++ www/js/minimo.apneaplus.js
@@ -0,0 +1,42 @@
+        document.addEventListener('deviceready', function () {
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            cordova.plugins.notification.local.on("click", function (notification) {
+                navigator.notification.confirm(
+                    'Hello ' + notification.data['username'] + ', Category selected ' + notification.id, // message
+                    onConfirm, // callback to invoke with index of button pressed
+                    'Saving data', // title
+                    ['Ok', 'Cancel'] // buttonLabels
+                );
+            });
+
+            function onConfirm(buttonIndex) {
+                switch(buttonIndex){
+                    case 1:
+                        navigator.notification.alert('Confirmed',function(){},'Selection');
+                        break;
+                    case 2:
+                        navigator.notification.alert('Canceled',function(){},'Selection');
+                        break;
+                }
+            }
+
+            testNotif = function () {
+                var now = new Date().getTime(),
+                    _5_sec_from_now = new Date(now + 5 * 1000);
+
+                var sound = "file://notification.mp3";
+
+                cordova.plugins.notification.local.schedule({
+                    id: 1,
+                    title: 'Apnea+',
+                    text: 'Dont forget to train today ...',
+                    at: _5_sec_from_now,
+                    sound: sound,
+                    badge: 1,
+                    data: {
+                        username: "Novan"
+                    }
+                })
+            };
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        });
